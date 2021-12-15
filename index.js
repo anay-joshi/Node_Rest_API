f731560d21cdffbf97aff5ceb2d76329e3fa0c37
Anay Joshi <anayjoshi1608@gmail.com>
2021-12-15 16:06:55 +0530

    Initial node rest API middleware

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to mongo");
+  }
+);
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/user", userRoute);
+
+/*
+app.get("/about", (req, res) => {
+  // res is response, req is request
+  res.send("Welcome to About!");
+});
+*/
+
+app.listen(8800, () => {
+  console.log("Backend server is running");
+});
